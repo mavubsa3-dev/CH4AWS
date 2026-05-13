a3cb5c18073749edad018653f91b2b5de5eb5c0e
mavubsa3-dev <mavubsa3@gmail.com>
2026-05-13 21:51:33 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/ch4aws/Ch4AwsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ch4aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch4AwsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch4AwsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ch4aws/Ch4AwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ch4aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ch4AwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
